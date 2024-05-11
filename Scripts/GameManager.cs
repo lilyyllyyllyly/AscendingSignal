cddab78eb91e9c72708a35ddaff31c593b2d04cb
Evelyn <143884194+its-Lyn@users.noreply.github.com>
2024-05-12 01:40:15 +0300

    Add signal, fix birds, spawn timers etc.

--- Scripts/GameManager.cs
+++ Scripts/GameManager.cs
@@ -4,16 +4,26 @@ using System;
 [GlobalClass]
 public partial class GameManager : Node
 {
+	[Signal] public delegate void ThirtyMetresTravelledEventHandler();
+	[Signal] public delegate void SixtyMetresTravelledEventHandler();
+
 	public static GameManager instance;
 
+	[Export] private Timer spawnTimer;
+
 	[Export] public Balloon[] balloons;
+	[Export] public ColorRect[] bars;
 
 	[Export] private PackedScene bird;
 	[Export] private Rect2 birdSpawnArea;
-	[Export] private float birdSpawnDelay;
-	private float lastBirdSpawn;
 
 	private RandomNumberGenerator rng;
+
+	private int currentBar = 0;
+
+	private int metresTravelled = 0;
+
+	private int birdSpawnChance = 7;
 
 	public override void _Ready()
 	{
@@ -26,18 +36,46 @@ public partial class GameManager : Node
 		rng = new RandomNumberGenerator();
 	}
 
-	public override void _Process(double delta)
-	{
-		float now = Time.GetTicksMsec();
-		if (now - lastBirdSpawn >= birdSpawnDelay) {
-			Node2D newBird = bird.Instantiate().GetNode<Node2D>(".");
-			GetTree().CurrentScene.AddChild(newBird);
+	private void SetBird() {
+		Node2D newBird = (Node2D)bird.Instantiate();
+		GetTree().CurrentScene.AddChild(newBird);
 
-			float posX = rng.RandfRange(birdSpawnArea.Position.X, birdSpawnArea.Position.X + birdSpawnArea.Size.X);
-			float posY = rng.RandfRange(birdSpawnArea.Position.Y, birdSpawnArea.Position.Y + birdSpawnArea.Size.Y);
-			newBird.GlobalPosition = new Vector2(posX, posY);
+		float posX = rng.RandfRange(birdSpawnArea.Position.X, birdSpawnArea.Position.X + birdSpawnArea.Size.X);
+		float posY = rng.RandfRange(birdSpawnArea.Position.Y, birdSpawnArea.Position.Y + birdSpawnArea.Size.Y);
+		newBird.GlobalPosition = new Vector2(posX, posY);
+	}
 
-			lastBirdSpawn = now;
+	private void SpawnBird() {
+		// Spawn one bird.
+		SetBird();
+		
+		// 1/birdSpawnChance chance for two birds to spawn.
+		// This decrements every 60 metres travelled.
+		if (Random.Shared.Next(0, birdSpawnChance) == birdSpawnChance - 1) {
+			SetBird();
+		}
+	}
+
+	private void OnMetreTravelled() {
+		metresTravelled += 1;
+
+		if (metresTravelled % 30 == 0) {
+			EmitSignal(SignalName.ThirtyMetresTravelled);
+			spawnTimer.WaitTime -= 0.2;
+		}
+
+		if (metresTravelled % 60 == 0) {
+			EmitSignal(SignalName.SixtyMetresTravelled);
+	
+			bars[currentBar].Color = new Color("34f35f");
+			currentBar += 1;
+			if (currentBar == bars.Length - 1) {
+				// TODO: kill the child.
+			}
+
+			// Cap spawn chance at 1/4
+			if (birdSpawnChance <= 4) return;
+			birdSpawnChance -= 1;
 		}
 	}
 }
