505291c39bebc1e6fcb0b0cbc2dbc60dda1f33e3
lilyyllyyllyly <lilylylith@gmail.com>
2024-05-11 20:11:53 -0300

    Add screenshake

--- Scripts/Camera.cs
+++ Scripts/Camera.cs
@@ -3,4 +3,23 @@ using System;
 
 public partial class Camera : Camera2D
 {
+	[Export] private float intensityMultiplier = 5;
+	private float currentIntensity;
+
+	public void Shake(float intensity)
+	{
+		currentIntensity = intensity;
+	}
+
+	public override void _Process(double delta)
+	{
+		currentIntensity -= (float)delta;
+		if (currentIntensity <= 0) {
+			Position = Vector2.Zero;
+			return;
+		}
+
+		float rot = Random.Shared.NextSingle() * Mathf.Tau;
+		Position = new Vector2(Mathf.Cos(rot), Mathf.Sin(rot)) * currentIntensity * intensityMultiplier;
+	}
 }
